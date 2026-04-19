4efa8d096269c20db3181a61294d997b8f695497
IZsolt8 <ivankovits.zsolt420@gmail.com>
2026-04-19 21:57:55 +0200

    migrations

---
+++ Robot_Simulation/Data/RobotSimulationContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Robot_Simulation.Models;
+
+namespace Robot_Simulation.Data
+{
+    public class RobotSimulationContext : DbContext
+    {
+        public RobotSimulationContext(DbContextOptions<RobotSimulationContext> options): base(options)
+        {
+        }
+        public DbSet<Game> Games { get; set; } = null!;
+        public DbSet<WareHouse> WareHouses { get; set; } = null!;
+    }
+}
---
+++ Robot_Simulation/Migrations/20260419194747_InitialCreate.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Robot_Simulation.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "WareHouses",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    StorgarSize = table.Column<int>(type: "int", nullable: false),
+                    MaitananceFee = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_WareHouses", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Games",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    GameName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Balance = table.Column<int>(type: "int", nullable: false),
+                    WarehouseId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Games", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_Games_WareHouses_WarehouseId",
+                        column: x => x.WarehouseId,
+                        principalTable: "WareHouses",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Games_WarehouseId",
+                table: "Games",
+                column: "WarehouseId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Games");
+
+            migrationBuilder.DropTable(
+                name: "WareHouses");
+        }
+    }
+}
